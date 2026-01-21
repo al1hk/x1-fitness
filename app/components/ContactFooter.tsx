e8f9bf9b2810ecb39bba9a596d370506f5bdf4df
al1hk <160332226+al1hk@users.noreply.github.com>
2026-01-22 02:18:00 +0500

    Update logo size in ContactFooter component

--- app/components/ContactFooter.tsx
+++ app/components/ContactFooter.tsx
@@ -65,7 +65,7 @@ const ContactFooter: React.FC = () => {
                         <img
                             src={logo.src}
                             alt="X1 Fitness"
-                            className="w-24 h-24 object-contain"
+                            className="w-28 h-28 md:w-32 md:h-32 object-contain"
                             loading="eager"
                             decoding="async"
                         />
